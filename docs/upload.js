d544b1f954b9a528e63dda405c1a0d619566de41
Macz-xiv <macz.xiv@gmail.com>
2025-07-18 00:26:01 +0330

    Update upload.js

--- docs/upload.js
+++ docs/upload.js
@@ -1,4 +1,4 @@
-const JWT = 'YOUR_FULL_SCOPED_PINATA_JWT'; // 🔐 Replace this
+const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MDU0ZjI0Ni1jMmZmLTRlZjQtYjVhZC1kNDE5NDQ2NGMyYTUiLCJlbWFpbCI6Im1hY3oueGl2QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI1YTUwMTFkY2FkODMxYzFkMzFlNyIsInNjb3BlZEtleVNlY3JldCI6IjI4MTQwMGNiYmI5ODMwNWNhYzk1YTU0NGI1NzE4YWIxYzU0NDI2OTAwZGQ5NzgyMWQ1NzdlZDI4ZGE3YTkyZTEiLCJleHAiOjE3ODQzMjE1ODl9.LWWZRPXfiCkKcEbJQvxQ8Ie0idHh-CKNjmoWjTd-VGE';
 
 export default async function uploadToIPFS(file) {
   const formData = new FormData();
@@ -7,7 +7,9 @@ export default async function uploadToIPFS(file) {
   try {
     const response = await fetch('https://api.pinata.cloud/pinning/pinFileToIPFS', {
       method: 'POST',
-      headers: { Authorization: `Bearer ${JWT}` },
+      headers: {
+        Authorization: `Bearer ${JWT}`
+      },
       body: formData
     });
 
